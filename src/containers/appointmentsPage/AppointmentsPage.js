6cac0b19c2cfea158c42fe8a5a42081925fc84d0
Chloe Harris <chloe.a.harris17@gmail.com>
2022-11-03 12:50:04 -0700

    Task seven, AppointmentsPage complete

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+import { AppointmentForm } from '../../components/appointmentForm/AppointmentForm';
+import { TileList } from '../../components/tileList/TileList';
 
+
+/* Recieve three props: list of appointments, contacts 
+  and the callback function for adding appointments*/
+
+export const AppointmentsPage = ({ 
+  appointments, 
+  contacts, 
+  addAppointment }) => {
+  // State variables holding the form’s appointment data
+  const [title, setTitle] = useState('');
+  const [contact, setContact] = useState(
+    contacts.length > 0 ? contacts[0].name : ''
+  );
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  
+  /* Add a new appointment and clear form on submission*/
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
+    addAppointment(title, contact, date, time);
+    setTitle('');
+    setContact('');
+    setDate('');
+    setTime('');
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+        contacts={contacts} 
+        title={title}
+        setTitle={setTitle}
+        contact={contact}
+        setContact={setContact}
+        date={date}
+        setDate={setDate}
+        time={time}
+        setTime={setTime}
+        handleSubmit={handleSubmit}/>
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList tiles={appointments}/>
       </section>
     </div>
   );
