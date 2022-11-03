abb2a23a29d0045cf7f8251c4a0d2f8dee44ca45
Chloe Harris <chloe.a.harris17@gmail.com>
2022-11-03 11:56:21 -0700

    Task five, TileList complete

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-export const TileList = () => {
+import { Tile } from "../tile/Tile";
+
+/* Receive one prop: an array of objects to render as a list */
+export const TileList = ({ tiles }) => {
   return (
     <div>
-      
+      {/* map callback has an object from the array
+      as the first parameter and that objects index
+      as the second parameter */}
+
+      {/* Return the Tile component with the object 
+      parameter passed as a prop (tile) and the index
+      parameter used as the components key */}
+      {tiles.map((tile, index) => (
+        <Tile key={index} tile={tile} />
+      ))}
     </div>
   );
 };
